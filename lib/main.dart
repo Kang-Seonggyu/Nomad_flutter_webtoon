451c613433baf7fe9cd75e793bdd284487c78572
Kang-Seonggyu <103415733+Kang-Seonggyu@users.noreply.github.com>
2024-05-27 10:01:38 +0900

    Feat: VSCode Settings

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,15 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Color(0xFF181818),
+          backgroundColor: const Color(0xFF181818),
           body: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 40,
+              ),
               child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    SizedBox(
+                    const SizedBox(
                       height: 80,
                     ),
                     Row(
@@ -24,7 +26,7 @@ class App extends StatelessWidget {
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.end,
                           children: [
-                            Text(
+                            const Text(
                               'Hey, Selena',
                               style: TextStyle(
                                 color: Colors.white,
@@ -43,7 +45,7 @@ class App extends StatelessWidget {
                         )
                       ],
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 120,
                     ),
                     Text(
@@ -53,17 +55,17 @@ class App extends StatelessWidget {
                         color: Colors.white.withOpacity(0.8),
                       ),
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 10,
                     ),
-                    Text(
+                    const Text(
                       '\$5 194 482',
                       style: TextStyle(
                           fontSize: 48,
                           fontWeight: FontWeight.w600,
                           color: Colors.white),
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 30,
                     ),
                     Row(children: [
@@ -71,7 +73,7 @@ class App extends StatelessWidget {
                         decoration: BoxDecoration(
                             color: Colors.amber,
                             borderRadius: BorderRadius.circular(45)),
-                        child: Padding(
+                        child: const Padding(
                           padding: EdgeInsets.symmetric(
                             vertical: 20,
                             horizontal: 50,
